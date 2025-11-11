1ff83112f610053e9d22ef14fb1a8d077aa11266
KyNguyen756 <ky2242004@gmail.com>
2025-11-11 21:16:39 +0700

    Update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = 'http://35.172.100.124:8080/api';
 const api = axios.create({ baseURL: API_URL });
 
 api.interceptors.request.use(config => {
